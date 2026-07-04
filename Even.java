ed1b5608b7d20905cc3ec5f46817566ed355fdd6
Gayathri272006 <gayathrihari2702@gmail.com>
2026-07-04 12:17:15 +0530

    Added Java practice folders

---
+++ Even.java
@@ -0,0 +1,11 @@
+public class Even {
+    public static void main(String[] args){ 
+    int n=24;
+    if(n%2==0){
+        System.out.println("Even");
+    }
+    else{
+        System.out.println("odd");
+    }
+}  
+}
